cb75ecdaba731a43dc3cea964e14a7f33d0d96bb
giovanni <giovannirafael0123@gmail.com>
2024-09-16 19:59:51 -0300

    primeiro commit - código de estudo POO

---
+++ pessoa.py
@@ -0,0 +1,52 @@
+class Pessoa:
+
+    def __init__(self, nome, idade, comendo=False, falando = False, ):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+    def comer(self, comida):
+        if self.comendo:
+            print(f'{self.nome} ja está comendo')
+            return
+        if self.falando:
+            print(f'{self.nome} não pode comer pois está falando')
+            return
+        print(f'{self.nome} está comendo {comida}')
+        self.comendo = True
+
+
+    def parar_comer(self):
+        if not self.comendo:
+            print(f'{self.nome} não está comendo')
+            return
+        print(f'{self.nome} parou de comer')
+        self.comendo = False
+
+
+    def falar(self, assunto):
+        if self.falando:
+            print(f'{self.nome} ja está falando')
+            return
+
+        if self.comendo:
+            print(f'{self.nome} não pode falar pois está comendo')
+            return
+
+        print(f'{self.nome} está falando sobre {assunto}')
+        self.falando = True
+
+
+    def parar_falar(self):
+        if not self.falar:
+            print(f'{self.nome} não entá falando')
+        print(f'{self.nome} parou de falar')
+        self.falando = False
+
+p1 = Pessoa('marcos', 23)
+
+p1.falar('video game')
+p1.comer('pizza')
+p1.parar_falar()
+p1.comer('pizza')
